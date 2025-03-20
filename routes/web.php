03a21ff7d182d083e84d7503741cc81fc7ff90e6
MR-7ava <mr_7ava@hotmail.com>
2025-03-20 11:05:12 -0300

    estudo sobre rotas

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/teste', function () {
+    return "o codigo esta tentando";
+});
+
+Route::get('/usuario/{nome}', function ($nome) {
+    return "usuario e atual ". $nome;
+});
+
+Route::get('/soma/{num1}/{num2}', function ($num1, $num2) {
+    return "A soma é". $num1 + $num2;
+});
+
+Route::get('/divisao/{num1}/{num2}', function ($num1, $num2) {
+    return "A divisao é". $num1 / $num2;
+});
+
+
